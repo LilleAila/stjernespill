cb5845f4617308ee9faf9707c3942581dcb7bec0
LilleAila <olai.solsvik@gmail.com>
2026-01-07 11:10:54 +0100

    fix paths

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import logo from "$lib/assets/logo.png";
+  import { base } from "$app/paths";
 </script>
 
 <nav id="nav">
   <img src={logo} alt="Stjernespill logo" />
   <ul>
-    <li><a href="/">Hjem</a></li>
-    <li><a href="/about">Om oss</a></li>
+    <li><a href="{base}">Hjem</a></li>
+    <li><a href="{base}/about">Om oss</a></li>
   </ul>
   <div></div>
 </nav>
