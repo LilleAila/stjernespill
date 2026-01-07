119c5e2d753e15b340e73d0449c1b6bf62de7ee6
LilleAila <LilleAila@users.noreply.github.com>
2026-01-07 10:23:30 +0000

    deploy: 7aa5558f15f2a6ce5fb4330963093b4e90aad4ac

---
+++ _app/immutable/chunks/D8RbOFEX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1xks2j4?.base??"/stjernespill",e=globalThis.__sveltekit_1xks2j4?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/Dj8_52ju.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_y2e1lg?.base??"/stjernespill",e=globalThis.__sveltekit_y2e1lg?.assets??s??"";export{e as a,s as b};
